21b5d466f8bdcfc3ed1cd7577367f9bf256b7e3b
avermeulen <andre.vermeulen@gmail.com>
2018-08-04 21:14:34 +0200

    added some comments

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ io.on('connection', function (client) {
         sendTo(client.id, msg);
     });
 
+    // when the dashboard user chat to a user
     client.on('chat-to', function (chatMessage) {
         let username = chatMessage.username;
         let message = chatMessage.message;
@@ -36,24 +37,29 @@ io.on('connection', function (client) {
         sendTo(socketId, message);
     });
 
+    // send a chat history list to a user
     client.on('get-chat-log', function (chatMessage) {
         let username = chatMessage.username;
         let chatLog = chats.chatLogForUserName(username);
         io.to(client.id).emit('chat-log', chatLog);
     });
 
+    // a new chat user login
     client.on('login', function (userData) {
         chats.login(client.id, userData);
+        // get a chat log for the user who is loggin in
         let chatLog = chats.chatLog(client.id);
-
+        // send the chat log to the user that is logging in
         io.to(client.id).emit('login-response', chatLog);
-
+        // tell the dashboard there is a new user
         io.to(dashboardSocketId).emit('new-user', userData.username);
-        
-        let msg = 'Hi, ' + userData.username + '! How can we help?';
+        // send a default message when a user login
+        let msg = 'Admin: Hi, ' + userData.username + '! How can we help?';
+        // send a message to a client
         sendTo(client.id, msg);
     });
 
+    // capture the socketId of the dashboard screen
     client.on('dashboard', function () {
         dashboardSocketId = client.id;
     });
